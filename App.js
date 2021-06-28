1f70fc6955431bd199f6237427af32d0e19c0683
Moniyaa <86552765+Moniyaa@users.noreply.github.com>
2021-06-28 10:08:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Info1 from "./components/Info1";
+
+export default function App() {
+  return (
+    <>
+    <Header />
+    <Footer />
+    <Info1 />  
+    <Info1 /> 
+    <Info1 /> 
+    <Info1 /> 
+    <Info1 /> 
+    <Info1 /> 
+    <Info1 /> 
+    <Info1 /> 
+    < />
+  );
+}
---
+++ Info1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info1(){
+  return (
+    <div className="note">
+    <h1>Web Development with React js</h1>
+    <p>Bootcamp starts from 3rd july at 6pm</p>
+    </div>
+  );
+  
+}
+export default Info1;
